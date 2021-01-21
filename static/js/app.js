b1e84f6d5549e6c80c6898ca35c20d77c452020d
Pratishtha Singh <pratishthasingh@MacBook-Pro.local>
2021-01-21 14:34:06 -0600

    updating to filter on all

--- static/js/app.js
+++ static/js/app.js
@@ -36,7 +36,7 @@ function updateFilters() {
       console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
-      console.log(filterId);
+      console.log('filterId',filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
